049889f4310382788e2a866e62a8ac880ecc916d
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2024-03-27 09:30:34 -0600

    default vars

--- terraform/feature/vars.tf
+++ terraform/feature/vars.tf
@@ -1,19 +1,27 @@
 variable "pingone_username" {
+  default = ""
 }
 variable "pingone_password" {
+  default = ""
 }
 variable "pingone_region" {
+  default = ""
 }
 variable "pingone_client_id" {
+  default = ""
 }
 variable "pingone_client_secret" {
+  default = ""
 }
 variable "pingone_environment_id" {
+  default = ""
 }
 variable "pingone_environment_name" {
   description = "name that will be used when creating PingOne Environment"
 }
 variable "pingone_environment_type" {
+  default = ""
 }
 variable "pingone_license_id" {
+  default = ""
 }
--- terraform/test/vars.tf
+++ terraform/test/vars.tf
@@ -1,20 +1,28 @@
 variable "pingone_username" {
+  default = ""
 }
 variable "pingone_password" {
+  default = ""
 }
 variable "pingone_region" {
+  default = ""
 }
 variable "pingone_client_id" {
+  default = ""
 }
 variable "pingone_client_secret" {
+  default = ""
 }
 variable "pingone_environment_id" {
+  default = ""
 }
 variable "pingone_environment_name" {
   description = "name that will be used when creating PingOne Environment"
-  default = "PingOne TEST Environment for Terraform"
+  default = "Platform-TEST"
 }
 variable "pingone_environment_type" {
+  default = ""
 }
 variable "pingone_license_id" {
+  default = ""
 }
